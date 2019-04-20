5a3c3904615468690a4de7d30710da54c080994d
Wenwei Liu <vickyliuwenwei@gmail.com>
2019-04-20 03:03:09 -0400

    update_data

--- code/Histogram.js
+++ code/Histogram.js
@@ -1,111 +1,129 @@
-var color = "steelblue";
+function parser(d) {
+    d.pMPG = +d.MPG;
+    return d;
+}
 
-// Generate a 1000 data points using normal distribution with mean=20, deviation=5
-var values = d3.range(1000).map(d3.random.normal(20, 5));
+function mpghist(csvdata) {
+    var binsize = 2;
+    var minbin = 36;
+    var maxbin = 60;
+    var numbins = (maxbin - minbin) / binsize;
 
-// A formatter for counts.
-var formatCount = d3.format(",.0f");
+    // whitespace on either side of the bars in units of MPG
+    var binmargin = .2; 
+    var margin = {top: 10, right: 30, bottom: 50, left: 60};
+    var width = 450 - margin.left - margin.right;
+    var height = 250 - margin.top - margin.bottom;
 
-var margin = {top: 20, right: 30, bottom: 30, left: 30},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    // Set the limits of the x axis
+    var xmin = minbin - 1
+    var xmax = maxbin + 1
 
-var max = d3.max(values);
-var min = d3.min(values);
-var x = d3.scale.linear()
-      .domain([min, max])
-      .range([0, width]);
+    histdata = new Array(numbins);
+    for (var i = 0; i < numbins; i++) {
+    histdata[i] = { numfill: 0, meta: "" };
+  }
 
-// Generate a histogram using twenty uniformly-spaced bins.
-var data = d3.layout.histogram()
-    .bins(x.ticks(20))
-    (values);
+  // Fill histdata with y-axis values and meta data
+    csvdata.forEach(function(d) {
+    var bin = Math.floor((d.pMPG - minbin) / binsize);
+    if ((bin.toString() != "NaN") && (bin < histdata.length)) {
+      histdata[bin].numfill += 1;
+      histdata[bin].meta += "<tr><td>" + d.City +
+        " " + d.State + 
+        "</td><td>" + 
+        d.pMPG.toFixed(1) + " mpg</td></tr>";
+    }
+    });
 
-var yMax = d3.max(data, function(d){return d.length});
-var yMin = d3.min(data, function(d){return d.length});
-var colorScale = d3.scale.linear()
-            .domain([yMin, yMax])
-            .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
+    // This scale is for determining the widths of the histogram bars
+    // Must start at 0 or else x(binsize a.k.a dx) will be negative
+    var x = d3.scale.linear()
+    .domain([0, (xmax - xmin)])
+    .range([0, width]);
 
-var y = d3.scale.linear()
-    .domain([0, yMax])
+    // Scale for the placement of the bars
+    var x2 = d3.scale.linear()
+    .domain([xmin, xmax])
+    .range([0, width]);
+  
+    var y = d3.scale.linear()
+    .domain([0, d3.max(histdata, function(d) { 
+            return d.numfill; 
+            })])
     .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
+    var xAxis = d3.svg.axis()
+    .scale(x2)
     .orient("bottom");
+    var yAxis = d3.svg.axis()
+    .scale(y)
+    .ticks(8)
+    .orient("left");
 
-var svg = d3.select("body").append("svg")
+    var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .direction('e')
+    .offset([0, 20])
+    .html(function(d) {
+      return '<table id="tiptable">' + d.meta + "</table>";
+  });
+
+    // put the graph in the "mpg" div
+    var svg = d3.select("#mpg").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + 
+            margin.top + ")");
 
-var bar = svg.selectAll(".bar")
-    .data(data)
-  .enter().append("g")
+    svg.call(tip);
+
+    // set up the bars
+    var bar = svg.selectAll(".bar")
+    .data(histdata)
+    .enter().append("g")
     .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+    .attr("transform", function(d, i) { return "translate(" + 
+         x2(i * binsize + minbin) + "," + y(d.numfill) + ")"; })
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide);
 
-bar.append("rect")
-    .attr("x", 1)
-    .attr("width", (x(data[0].dx) - x(0)) - 1)
-    .attr("height", function(d) { return height - y(d.y); })
-    .attr("fill", function(d) { return colorScale(d.y) });
+    // add rectangles of correct size at correct location
+    bar.append("rect")
+    .attr("x", x(binmargin))
+    .attr("width", x(binsize - 2 * binmargin))
+    .attr("height", function(d) { return height - y(d.numfill); });
 
-bar.append("text")
-    .attr("dy", ".75em")
-    .attr("y", -12)
-    .attr("x", (x(data[0].dx) - x(0)) / 2)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatCount(d.y); });
-
-svg.append("g")
+    // add the x axis and x-label
+    svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
+    svg.append("text")
+    .attr("class", "xlabel")
+    .attr("text-anchor", "middle")
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom)
+    .text("MPG");
 
-/*
-* Adding refresh method to reload new data
-*/
-function refresh(values){
-  // var values = d3.range(1000).map(d3.random.normal(20, 5));
-  var data = d3.layout.histogram()
-    .bins(x.ticks(20))
-    (values);
-
-  // Reset y domain using new data
-  var yMax = d3.max(data, function(d){return d.length});
-  var yMin = d3.min(data, function(d){return d.length});
-  y.domain([0, yMax]);
-  var colorScale = d3.scale.linear()
-              .domain([yMin, yMax])
-              .range([d3.rgb(color).brighter(), d3.rgb(color).darker()]);
-
-  var bar = svg.selectAll(".bar").data(data);
-
-  // Remove object with data
-  bar.exit().remove();
-
-  bar.transition()
-    .duration(1000)
-    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-
-  bar.select("rect")
-      .transition()
-      .duration(1000)
-      .attr("height", function(d) { return height - y(d.y); })
-      .attr("fill", function(d) { return colorScale(d.y) });
-
-  bar.select("text")
-      .transition()
-      .duration(1000)
-      .text(function(d) { return formatCount(d.y); });
-
+    // add the y axis and y-label
+    svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(0,0)")
+    .call(yAxis);
+    svg.append("text")
+    .attr("class", "ylabel")
+    .attr("y", 0 - margin.left) // x and y switched due to rotation
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "middle")
+    .text("# of fill-ups");
 }
 
-// Calling refresh repeatedly.
-setInterval(function() {
-  var values = d3.range(1000).map(d3.random.normal(20, 5));
-  refresh(values);
-}, 2000);
+// Read in .csv data and make graph
+d3.csv("https://vickyliuwenwei.github.io/VA_Gerrymandering/data/17_mms.csv", parser,
+       function(error, csvdata) {
+     mpghist(csvdata);
+});
