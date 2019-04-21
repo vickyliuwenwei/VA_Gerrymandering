49cffd7736de315941058ab369ca893a33db52e3
Wenwei Liu <vickyliuwenwei@gmail.com>
2019-04-21 04:22:39 -0400

    Update Histogram.js

--- code/Histogram.js
+++ code/Histogram.js
@@ -9,11 +9,15 @@ var format = d3.time.format("%m/%d/%Y");
 
 function mpghist(csvdata) {
   var maxbin = d3.max(csvdata, function(d) { return d.MPG; });
+  console.log(maxbin);
   var minbin = d3.min(csvdata, function(d) { return d.MPG; });
+  console.log(minbin);
   var numbins = 10;
   var binsize = (maxbin - minbin)/numbins;
+  console.log(binsize);
   // var minbin = 36;
   // var maxbin = 60;
+  // var binsize = 2;
   // var numbins = (maxbin - minbin) / binsize;
   // whitespace on either side of the bars in units of MPG
   var binmargin = .2; 
