0b4cb61eee6ab62c54014f1c20f82b03710e78e7
Wenwei Liu <vickyliuwenwei@gmail.com>
2019-04-21 03:53:27 -0400

    update_data

--- code/Histogram.js
+++ code/Histogram.js
@@ -2,164 +2,55 @@
 
 function parser(d) {
     d.pMPG = +d.MPG;
-    d.pOdometer = +d.Odometer;
-    d.pDate = new Date(d.Date);
+    // d.pOdometer = +d.Odometer;
+    // d.pDate = new Date(d.Date);
     return d;
 }
 
 var format = d3.time.format("%m/%d/%Y");
 
-// function milesovertime(csvdata) {
-//     var margin = {top: 30, right: 30, bottom: 100, left: 100};
-//     var width = 1000 - margin.left - margin.right;
-//     var height = 600 - margin.top - margin.bottom;
-
-//     var minDate = csvdata[0].pDate;
-//     var maxDate = csvdata[csvdata.length - 1].pDate;
-
-//     // Set up time based x axis
-//     var x = d3.time.scale()
-//   .domain([minDate, maxDate])
-//   .range([0, width]);
-
-//     var y = d3.scale.linear()
-//   .domain([0, d3.max(csvdata, function(d) { return d.pOdometer; })])
-//   .range([height, 0]);
-
-//     var xAxis = d3.svg.axis()
-//   .scale(x)
-//   .ticks(10)
-//   .orient("bottom");
-
-//     var yAxis = d3.svg.axis()
-//   .scale(y)
-//   .ticks(7)
-//   .orient("left");
-
-//     // put the graph in the "miles" div
-//     var svg = d3.select("#miles").append("svg")
-//   .attr("width", width + margin.left + margin.right)
-//   .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     // function to draw the line
-//     var line = d3.svg.line()
-//   .x(function(d) { return x(d.pDate); } )
-//   .y(function(d) { return y(d.pOdometer); } );
-
-//     //Mouseover tip
-//     var tip = d3.tip()
-//   .attr('class', 'd3-tip')
-//   .offset([120, 40])
-//   .html(function(d) {
-//       return "<strong>" + d.Odometer + " miles</strong><br>" +
-//     d.MPG + " mpg" + "<br>" +
-//     format(d.pDate) + "<br>" + 
-//     d.Brand + ", " + d.City + " " + d.State + "<br>";
-//   });
-
-//     svg.call(tip);
-
-//     // add the x axis and x-label
-//     svg.append("g")
-//   .attr("class", "x axis")
-//   .attr("transform", "translate(0," + height + ")")
-//   .call(xAxis)
-//   .selectAll("text")
-//   .attr("y", 9)
-//   .attr("x", 9)
-//   .attr("dy", ".35em")
-//   .attr("transform", "rotate(45)")
-//   .style("text-anchor", "start");
-//     svg.append("text")
-//   .attr("class", "xlabel")
-//   .attr("text-anchor", "middle")
-//   .attr("x", width / 2)
-//   .attr("y", height + margin.bottom)
-//   .text("Month");
-
-//     // add the y axis and y-label
-//     svg.append("g")
-//   .attr("class", "y axis")
-//   .attr("transform", "translate(0,0)")
-//   .call(yAxis);
-//     svg.append("text")
-//   .attr("class", "ylabel")
-//   .attr("y", 0 - margin.left) // x and y switched due to rotation!!
-//   .attr("x", 0 - (height / 2))
-//   .attr("dy", "1em")
-//   .attr("transform", "rotate(-90)")
-//   .style("text-anchor", "middle")
-//   .text("Odometer reading (mi)");
-
-//     svg.append("text")
-//   .attr("class", "graphtitle")
-//   .attr("y", 30)
-//   .attr("x", width/2)
-//   .style("text-anchor", "middle")
-//   .text("MILES OVER TIME");
-
-//     // draw the line
-//     svg.append("path")
-//   .attr("d", line(csvdata));
-
-//     svg.selectAll(".dot")
-//   .data(csvdata)
-//   .enter().append("circle")
-//   .attr('class', 'datapoint')
-//   .attr('cx', function(d) { return x(d.pDate); })
-//   .attr('cy', function(d) { return y(d.pOdometer); })
-//   .attr('r', 6)
-//   .attr('fill', 'white')
-//   .attr('stroke', 'steelblue')
-//   .attr('stroke-width', '3')
-//   .on('mouseover', tip.show)
-//   .on('mouseout', tip.hide);
-// }
-
 function mpghist(csvdata) {
-    var binsize = 2;
-    var minbin = 36;
-    var maxbin = 60;
-    var numbins = (maxbin - minbin) / binsize;
+  var binsize = 2;
+  var minbin = 36;
+  var maxbin = 60;
+  var numbins = (maxbin - minbin) / binsize;
+  // whitespace on either side of the bars in units of MPG
+  var binmargin = .2; 
+  var margin = {top: 10, right: 30, bottom: 50, left: 60};
+  var width = 450 - margin.left - margin.right;
+  var height = 250 - margin.top - margin.bottom;
 
-    var binmargin = .2; // whitespace on either side of the bars in units of MPG
-    var margin = {top: 10, right: 30, bottom: 50, left: 60};
-    var width = 450 - margin.left - margin.right;
-    var height = 250 - margin.top - margin.bottom;
+  // Set the limits of the x axis
+  var xmin = minbin - 1
+  var xmax = maxbin + 1
 
-    // Set the limits of the x axis
-    var xmin = minbin - 1
-    var xmax = maxbin + 1
-
-    histdata = new Array(numbins);
-    for (var i = 0; i < numbins; i++) {
-  if (i < histdata.length) {
+  histdata = new Array(numbins);
+  for (var i = 0; i < numbins; i++) {
+    if (i < histdata.length) {
       histdata[i] = { numfill: 0, meta: "" };
     }
-    }
+  }
 
-    csvdata.forEach(function(d) {
+  csvdata.forEach(function(d) {
   var bin = Math.floor((d.pMPG - minbin) / binsize);
-  if ((bin.toString() != "NaN") && (bin < histdata.length)) {
-      histdata[bin].numfill += 1;
-      histdata[bin].meta += "<tr><td>" + d.City + " " + d.State + 
-    "</td><td>" + format(d.pDate) + 
-    "</td><td>" + d.pMPG.toFixed(1) + " mpg</td></tr>";
-    }
-    });
+  // if ((bin.toString() != "NaN") && (bin < histdata.length)) {
+  //     histdata[bin].numfill += 1;
+  //     histdata[bin].meta += "<tr><td>" + d.City + " " + d.State + 
+  //   "</td><td>" + format(d.pDate) + 
+  //   "</td><td>" + d.pMPG.toFixed(1) + " mpg</td></tr>";
+  //   }
+  });
 
     // This scale is for determining the widths of the histogram bars
     // Must start at 0 or else x(binsize a.k.a dx) will be negative
     var x = d3.scale.linear()
-  .domain([0, (xmax - xmin)])
-  .range([0, width]);
+    .domain([0, (xmax - xmin)])
+    .range([0, width]);
 
     // Scale for the placement of the bars
     var x2 = d3.scale.linear()
-  .domain([xmin, xmax])
-  .range([0, width]);
+    .domain([xmin, xmax])
+    .range([0, width]);
 
     // Make an array with the mpg values
     var values = [];
@@ -177,13 +68,13 @@ function mpghist(csvdata) {
   .ticks(8)
   .orient("left");
 
-    var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .direction('e')
-  .offset([0, 20])
-  .html(function(d) {
-      return '<table id="tiptable">' + d.meta + "</table>";
-  });
+  //   var tip = d3.tip()
+  // .attr('class', 'd3-tip')
+  // .direction('e')
+  // .offset([0, 20])
+  // .html(function(d) {
+  //     return '<table id="tiptable">' + d.meta + "</table>";
+  // });
 
     // put the graph in the "mpg" div
     var svg = d3.select("#mpg").append("svg")
@@ -238,7 +129,7 @@ function mpghist(csvdata) {
 
 // Read in .csv data and make graphs
 d3.csv("data/prius_gas.csv", parser,
-       function(error, csvdata) {
-     mpghist(csvdata);
+  function(error, csvdata) {
+    mpghist(csvdata);
      // milesovertime(csvdata);
 }); // end of d3.csv
