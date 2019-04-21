e766d477bbf63563ee813d8e24ce83551d7b0e94
Wenwei Liu <vickyliuwenwei@gmail.com>
2019-04-21 03:43:17 -0400

    Update Histogram.js

--- code/Histogram.js
+++ code/Histogram.js
@@ -9,114 +9,114 @@ function parser(d) {
 
 var format = d3.time.format("%m/%d/%Y");
 
-function milesovertime(csvdata) {
-    var margin = {top: 30, right: 30, bottom: 100, left: 100};
-    var width = 1000 - margin.left - margin.right;
-    var height = 600 - margin.top - margin.bottom;
+// function milesovertime(csvdata) {
+//     var margin = {top: 30, right: 30, bottom: 100, left: 100};
+//     var width = 1000 - margin.left - margin.right;
+//     var height = 600 - margin.top - margin.bottom;
 
-    var minDate = csvdata[0].pDate;
-    var maxDate = csvdata[csvdata.length - 1].pDate;
+//     var minDate = csvdata[0].pDate;
+//     var maxDate = csvdata[csvdata.length - 1].pDate;
 
-    // Set up time based x axis
-    var x = d3.time.scale()
-  .domain([minDate, maxDate])
-  .range([0, width]);
+//     // Set up time based x axis
+//     var x = d3.time.scale()
+//   .domain([minDate, maxDate])
+//   .range([0, width]);
 
-    var y = d3.scale.linear()
-  .domain([0, d3.max(csvdata, function(d) { return d.pOdometer; })])
-  .range([height, 0]);
+//     var y = d3.scale.linear()
+//   .domain([0, d3.max(csvdata, function(d) { return d.pOdometer; })])
+//   .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-  .scale(x)
-  .ticks(10)
-  .orient("bottom");
+//     var xAxis = d3.svg.axis()
+//   .scale(x)
+//   .ticks(10)
+//   .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-  .scale(y)
-  .ticks(7)
-  .orient("left");
+//     var yAxis = d3.svg.axis()
+//   .scale(y)
+//   .ticks(7)
+//   .orient("left");
 
-    // put the graph in the "miles" div
-    var svg = d3.select("#miles").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//     // put the graph in the "miles" div
+//     var svg = d3.select("#miles").append("svg")
+//   .attr("width", width + margin.left + margin.right)
+//   .attr("height", height + margin.top + margin.bottom)
+//   .append("g")
+//   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // function to draw the line
-    var line = d3.svg.line()
-  .x(function(d) { return x(d.pDate); } )
-  .y(function(d) { return y(d.pOdometer); } );
+//     // function to draw the line
+//     var line = d3.svg.line()
+//   .x(function(d) { return x(d.pDate); } )
+//   .y(function(d) { return y(d.pOdometer); } );
 
-    //Mouseover tip
-    var tip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([120, 40])
-  .html(function(d) {
-      return "<strong>" + d.Odometer + " miles</strong><br>" +
-    d.MPG + " mpg" + "<br>" +
-    format(d.pDate) + "<br>" + 
-    d.Brand + ", " + d.City + " " + d.State + "<br>";
-  });
+//     //Mouseover tip
+//     var tip = d3.tip()
+//   .attr('class', 'd3-tip')
+//   .offset([120, 40])
+//   .html(function(d) {
+//       return "<strong>" + d.Odometer + " miles</strong><br>" +
+//     d.MPG + " mpg" + "<br>" +
+//     format(d.pDate) + "<br>" + 
+//     d.Brand + ", " + d.City + " " + d.State + "<br>";
+//   });
 
-    svg.call(tip);
+//     svg.call(tip);
 
-    // add the x axis and x-label
-    svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis)
-  .selectAll("text")
-  .attr("y", 9)
-  .attr("x", 9)
-  .attr("dy", ".35em")
-  .attr("transform", "rotate(45)")
-  .style("text-anchor", "start");
-    svg.append("text")
-  .attr("class", "xlabel")
-  .attr("text-anchor", "middle")
-  .attr("x", width / 2)
-  .attr("y", height + margin.bottom)
-  .text("Month");
+//     // add the x axis and x-label
+//     svg.append("g")
+//   .attr("class", "x axis")
+//   .attr("transform", "translate(0," + height + ")")
+//   .call(xAxis)
+//   .selectAll("text")
+//   .attr("y", 9)
+//   .attr("x", 9)
+//   .attr("dy", ".35em")
+//   .attr("transform", "rotate(45)")
+//   .style("text-anchor", "start");
+//     svg.append("text")
+//   .attr("class", "xlabel")
+//   .attr("text-anchor", "middle")
+//   .attr("x", width / 2)
+//   .attr("y", height + margin.bottom)
+//   .text("Month");
 
-    // add the y axis and y-label
-    svg.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(0,0)")
-  .call(yAxis);
-    svg.append("text")
-  .attr("class", "ylabel")
-  .attr("y", 0 - margin.left) // x and y switched due to rotation!!
-  .attr("x", 0 - (height / 2))
-  .attr("dy", "1em")
-  .attr("transform", "rotate(-90)")
-  .style("text-anchor", "middle")
-  .text("Odometer reading (mi)");
+//     // add the y axis and y-label
+//     svg.append("g")
+//   .attr("class", "y axis")
+//   .attr("transform", "translate(0,0)")
+//   .call(yAxis);
+//     svg.append("text")
+//   .attr("class", "ylabel")
+//   .attr("y", 0 - margin.left) // x and y switched due to rotation!!
+//   .attr("x", 0 - (height / 2))
+//   .attr("dy", "1em")
+//   .attr("transform", "rotate(-90)")
+//   .style("text-anchor", "middle")
+//   .text("Odometer reading (mi)");
 
-    svg.append("text")
-  .attr("class", "graphtitle")
-  .attr("y", 30)
-  .attr("x", width/2)
-  .style("text-anchor", "middle")
-  .text("MILES OVER TIME");
+//     svg.append("text")
+//   .attr("class", "graphtitle")
+//   .attr("y", 30)
+//   .attr("x", width/2)
+//   .style("text-anchor", "middle")
+//   .text("MILES OVER TIME");
 
-    // draw the line
-    svg.append("path")
-  .attr("d", line(csvdata));
+//     // draw the line
+//     svg.append("path")
+//   .attr("d", line(csvdata));
 
-    svg.selectAll(".dot")
-  .data(csvdata)
-  .enter().append("circle")
-  .attr('class', 'datapoint')
-  .attr('cx', function(d) { return x(d.pDate); })
-  .attr('cy', function(d) { return y(d.pOdometer); })
-  .attr('r', 6)
-  .attr('fill', 'white')
-  .attr('stroke', 'steelblue')
-  .attr('stroke-width', '3')
-  .on('mouseover', tip.show)
-  .on('mouseout', tip.hide);
-}
+//     svg.selectAll(".dot")
+//   .data(csvdata)
+//   .enter().append("circle")
+//   .attr('class', 'datapoint')
+//   .attr('cx', function(d) { return x(d.pDate); })
+//   .attr('cy', function(d) { return y(d.pOdometer); })
+//   .attr('r', 6)
+//   .attr('fill', 'white')
+//   .attr('stroke', 'steelblue')
+//   .attr('stroke-width', '3')
+//   .on('mouseover', tip.show)
+//   .on('mouseout', tip.hide);
+// }
 
 function mpghist(csvdata) {
     var binsize = 2;
@@ -240,5 +240,5 @@ function mpghist(csvdata) {
 d3.csv("data/prius_gas.csv", parser,
        function(error, csvdata) {
      mpghist(csvdata);
-     milesovertime(csvdata);
+     // milesovertime(csvdata);
 }); // end of d3.csv
